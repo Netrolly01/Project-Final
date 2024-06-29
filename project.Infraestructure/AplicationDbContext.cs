78688b3abdcd682bc649ee0960018f931ec4e2cf
danielthomas.xd99@outlook.com <danielthomas.xd99@outlook.com>
2024-06-29 14:42:08 -0400

    Aplication Db Contex

---
+++ project.Infraestructure/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolActivityApp.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SchoolActivityApp.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<ExtraActivity> ExtraActivities { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
