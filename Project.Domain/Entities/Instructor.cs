80f6d86d51b0bb49551664e14edc702723e928aa
Carlos Reynoso <malacacatuana5@gmail.com>
2024-06-29 12:19:37 -0400

    en Entities añadi las prop de Instructor y de Student

--- Project.Domain/Entities/Instructor.cs
+++ Project.Domain/Entities/Instructor.cs
@@ -8,5 +8,10 @@ namespace SchoolActivityApp.Domain.Entities
 {
     public class Instructor
     {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set;}
+        public string Especializacion { get; set; }
     }
 }
--- Project.Domain/Entities/Student.cs
+++ Project.Domain/Entities/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolActivityApp.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,12 @@ namespace SchoolActivityApp.Domain.Entities
     public class student
 
     {
+    public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Grado { get; set;}
+        public string Direccion { get; set;}
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+
     }
 }
