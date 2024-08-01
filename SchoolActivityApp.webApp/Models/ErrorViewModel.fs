5fb3040a17aaa1814b8814f39db4de766baa42b1
danielthomas.xd99@outlook.com <danielthomas.xd99@outlook.com>
2024-07-31 20:45:55 -0400

    prueba

---
+++ SchoolActivityApp.webApp/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace SchoolActivityApp.webApp.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
